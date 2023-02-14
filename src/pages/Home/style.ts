ba17f8f42835e38ba90075361b1958db30bf1995
Nathan Dias <nathanfaria9@gmail.com>
2023-02-14 08:25:14 -0300

    Home commit

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,61 +1,64 @@
 import styled from "styled-components";
-import backgroundBurguer from '../../assets/backgroundBurguer.jpg';
-import logo from '../../assets/logo.png';
+import backgroundBurguer from "../../assets/backgroundBurguer.jpg";
+import logo from "../../assets/logo.png";
 
 export const HomeContainer = styled.main`
-    width: 100%;
-    height: 100vh;
-`
+  width: 100%;
+  height: 100vh;
+`;
 
 export const HomeBackground = styled.div`
-    height: 100%;
-    background-image: url(${backgroundBurguer});
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-`
+  height: 100%;
+  background-image: url(${backgroundBurguer});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  @media (max-width: 879px) {
+    justify-content: center;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5),
+                       rgba(0, 0, 0, 0.5)),url(${backgroundBurguer});
+  }
+`;
 
 export const HomeDescript = styled.div`
-    width: 540px;
-    height: 400px;
-    color: #fff;
-    display: flex;
-    flex-direction:column;
-    align-items: center;
-    justify-content: center;
-    gap: 0.4rem;
-`
+  width: 540px;
+  height: 400px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+`;
 
-export const LogoContainer = styled.div`
-`
+export const LogoContainer = styled.div``;
 
 export const Logo = styled.img`
-    height: 6rem;
-`
-
+  height: 6rem;
+`;
 
 export const DescriptText = styled.div`
-    font-size: 1.4rem;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 0.8rem;
+  font-size: 1.4rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.8rem;
 `;
 
 export const ButtonAccess = styled.button`
-    margin-top: 1rem;
-    padding-inline: 1.6rem;
-    padding-block: 1rem;
-    font-size: 1.2rem;
-    border: 1px solid #fff3;
-    background-color: transparent;
-    color: #fff;
-    cursor: pointer;
-    
-    &:hover{
-        background-color: #fff1;
-    }
+  margin-top: 1rem;
+  padding-inline: 1.6rem;
+  padding-block: 1rem;
+  font-size: 1.2rem;
+  border: 1px solid #fff3;
+  background-color: transparent;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fff1;
+  }
 `;
